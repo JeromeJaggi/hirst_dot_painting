e0e7c6e60b117615d3de0e2a9f3728404ddec3ff
JeromeJaggi <81522441+JeromeJaggi@users.noreply.github.com>
2021-07-12 21:30:38 +0100

    Upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import colorgram
+
+# rgb_colors =[]
+# colors = colorgram.extract("image.jpg", 8)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#     print(rgb_colors)
+
+
+color_list = [(231, 251, 242), (198, 12, 32), (250, 237, 17), (39, 77, 189), (38, 217, 67), (238, 228, 5)]
